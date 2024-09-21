db7a9d94a98c90ab0fdf5e16df4280633b308f69
Golam Sarowar <86582538+gsBullet@users.noreply.github.com>
2024-09-21 19:23:58 +0600

    add section complete and animation done

--- public/assects/js/app.js
+++ public/assects/js/app.js
@@ -1,3 +1,15 @@
+// toggle button 
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const linksContainer = document.querySelector(".links-container");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+  linksContainer.classList.toggle("show");
+});
+
+// kinks 
+
 const links = document.querySelectorAll(".link");
 
 links.forEach((link) => {
@@ -5,4 +17,39 @@ links.forEach((link) => {
     links.forEach((ele) => ele.classList.remove("active"));
     link.classList.add("active");
   });
+});
+
+const btn = document.querySelector(".contact-btn");
+const firstName = document.querySelector(".first-name");
+const lastName = document.querySelector(".last-name");
+const email = document.querySelector(".email");
+const message = document.querySelector(".message");
+
+btn.addEventListener("click", async () => {
+  if (
+    firstName.value.length &&
+    lastName.value.length &&
+    email.value.length &&
+    message.value.length
+  ) {
+    await fetch("/mail", {
+      method: "post",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        message: message.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+       return alert(data);
+      })
+      .catch((err) => console.error(err));
+  } else {
+    return alert("Please fill in all fields");
+  }
 });
--- server.js
+++ server.js
@@ -1,16 +1,64 @@
+// server.js or app.js
+
+require("dotenv").config(); // Load environment variables
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const path = require("path");
-const port = 3000;
+const nodemailer = require("nodemailer");
 
-let initialPath = path.join(__dirname, "public");
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the "public" directory
+const initialPath = path.join(__dirname, "public");
 app.use(express.static(initialPath));
 
+// Serve the index.html at the root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(initialPath, "index.html"));
 });
 
+app.post("/mail", (req, res) => {
+  const { firstName, lastName, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    logger: true,
+    debug: true,
+  });
+
+  const mailOptions = {
+    from: `${process.env.EMAIL}`,
+    to: `${email}`,
+    subject: "portfolio submission",
+    text: `First Name: ${firstName}\nLast Name: ${lastName} \nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json("Failed to send email");
+    } else {
+      return res
+        .status(200)
+        .json(
+          "Email sent successfully, I will replay to you within 2 working days"
+        );
+    }
+  });
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log("Server is running on port 3000"); // Server is listening on port 3000
+  console.log(`Server is running on port ${port}`);
 });
