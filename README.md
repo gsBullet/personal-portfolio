90bfa583cfb66a2ef2b4d040ab52dd961be1a463
Golam Sarowar <86582538+gsBullet@users.noreply.github.com>
2024-09-27 15:26:42 +0600

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# npm install
+
+# npm start
+
+# netlify dev
+
+# netlify deploy --prod
+
+# touch netlify/functions/sendMail.js
+
+# mkdir -p netlify/functions
--- public/assects/js/project.js
+++ public/assects/js/project.js
@@ -1,36 +1,93 @@
 let projects = [
   {
     name: "project one",
-    tags: "#javaScript, #fullStack , #PSDtoHTML, #backend",
-    image: "1.jpg",
+    tags: "#javaScript, #fullStack , #PSDtoHTML, #backend #HTML_CSS",
+    image: "all.png",
+    link: "#",
   },
   {
     name: "project two",
-    tags: "#javaScript, #fullStack",
-    image: "2.jpg",
+    tags: "#html_css",
+    image: "1.png",
+    link: "https://gsbullet.github.io/nested_nested_navbar/",
   },
   {
     name: "project three",
-    tags: " #PSDtoHTML",
-    image: "3.jpg",
+    tags: "#html_css",
+    image: "2.png",
+    link: "https://gsbullet.github.io/personal_cv/",
+  },
+  {
+    name: "project four",
+    tags: "#html_css",
+    image: "3.png",
+    link: "https://gsbullet.github.io/windows_8_menu/index.html",
+  },
+  {
+    name: "project four",
+    tags: "#html_css #responsive",
+    image: "4.png",
+    link: "https://gsbullet.github.io/responsive_webpage/",
+  },
+
+  {
+    name: "project five",
+    tags: " #PSDtoHTML,#html_css",
+    image: "psd.png",
+    link: "https://gsbullet.github.io/meat_house_web/",
+  },
+  {
+    name: "project six",
+    tags: "#html_css",
+    image: "port.png",
+    link: "https://gsbullet.github.io/personal_portfolio/",
+  },
+  {
+    name: "project four",
+    tags: "#html_css",
+    image: "praroz.png",
+    link: "https://gsbullet.github.io/praroz_web_design/",
   },
   {
     name: "project four",
     tags: " #backend , #javaScript",
-    image: "4.jpg",
+    image: "all.png",
+    link: "",
+  },
+  {
+    name: "project four",
+    tags: " #backend , #javaScript",
+    image: "all.png",
+    link: "",
+  },
+  {
+    name: "project four",
+    tags: " #backend , #javaScript",
+    image: "all.png",
+    link: "",
+  },
+  {
+    name: "project four",
+    tags: " #backend , #javaScript",
+    image: "all.png",
+    link: "",
   },
 ];
 
 let projectContainer = document.querySelector(".project-container");
 
 projects.forEach((project) => {
-  projectContainer.innerHTML += `<div class="project-card " data-tags="#all, ${project.tags}">
+  projectContainer.innerHTML += `
+  <a class="project-link" href="${project.link}">
+  <div class="project-card " data-tags="#all, ${project.tags}">
         <img src="./assects/img/${project.image}" alt="" class="project-image" />
         <div class="project-content">
           <h3 class="project-title">${project.name}</h3>
           <span class="tags">${project.tags}</span>
         </div>
-      </div>`;
+      </div>
+      </a>
+      `;
 });
 
 const filters = document.querySelectorAll(".filter-btn");
